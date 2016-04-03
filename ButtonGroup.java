32f694978323cb05a191ab0d3d573f2d7adbf08d
S <s>
2016-04-03 12:19:33 -0700

    {"types":"C","runs":4}

--- ButtonGroup.java
+++ ButtonGroup.java
@@ -34,6 +34,20 @@ public class ButtonGroup extends MouseWidget
 		return buttons.get(instr);
 	}
 	
+	public void enableOutline(double width, Color lineColor)
+	{
+		if(outline != null) remove(outline);
+		outline = base;
+		outline.setFillColor(lineColor);
+		this.lineColor = lineColor;
+		base = new GRect(getWidth() - 2 * width, getHeight() - 2 * width);
+		base.setFilled(true);
+		base.setFillColor(baseColor);
+		add(base, width, width);
+		base.sendToBack();
+		outline.sendToBack();
+	}
+	
 	public void mousePressed(MouseEvent e)
 	{
 		GObject o = getElementAt(e.getX(), e.getY());
@@ -49,4 +63,6 @@ public class ButtonGroup extends MouseWidget
 	private Map<String, GenericButton> buttons;
 	private Color baseColor;
 	private GRect base;
+	private GRect outline;
+	private Color lineColor;
 }
