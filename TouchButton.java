3c00e60732a551c43ae4c7bed3498425a8c00447
S <s>
2016-04-02 19:27:26 -0700

    {"types":"C","runs":15}

--- TouchButton.java
+++ TouchButton.java
@@ -1,7 +1,19 @@
-
-
+import java.awt.Color;
 
 public class TouchButton extends GenericButton
 {
-
+	public TouchButton(double width, double height, Color baseColor, String instr)
+	{
+		super(width, height, baseColor, instr);
+	}
+	
+	public void pressButton()
+	{
+		rect.setColor(baseColor.brighter());
+	}
+	
+	public void releaseButton()
+	{
+		rect.setColor(baseColor.darker());
+	}
 }
