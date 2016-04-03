2c027bd78cd888721c096f0d54ad122977a18d1a
S <s>
2016-04-02 20:06:44 -0700

    {"types":"C","runs":19}

--- ButtonGroup.java
+++ ButtonGroup.java
@@ -8,8 +8,8 @@ public class ButtonGroup extends GCompound
 {
 	public ButtonGroup(double width, double height)
 	{
-		add(new TouchButton(50, 100, Color.RED, "RCL"), 10, 10);
-		add(new TouchButton(50, 100, Color.RED, "RCL"), 10, 70);
+		add(new TouchButton(100, 50, Color.RED, "RCL"), 10, 10);
+		add(new TouchButton(100, 50, Color.RED, "RCL"), 10, 70);
 	}
 	
 	private List<GenericButton> buttons;
