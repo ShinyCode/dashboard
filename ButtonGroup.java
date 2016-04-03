3078d939621868b4b8395c2fffdef8d0283387cb
S <s>
2016-04-02 20:14:32 -0700

    {"types":"C","runs":20}

--- ButtonGroup.java
+++ ButtonGroup.java
@@ -8,8 +8,29 @@ public class ButtonGroup extends GCompound
 {
 	public ButtonGroup(double width, double height)
 	{
-		add(new TouchButton(100, 50, Color.RED, "RCL"), 10, 10);
-		add(new TouchButton(100, 50, Color.RED, "RCL"), 10, 70);
+		
+	}
+	
+	public void mousePressed(double x, double y)
+	{
+		GObject o = getElementAt(x, y);
+		if(o instanceof TouchButton)
+		{
+			((TouchButton) o).pressButton();
+		}
+		else if(o instanceof ToggleButton)
+		{
+			((ToggleButton) o).toggleButton();
+		}
+	}
+	
+	public void mouseReleased(double x, double y)
+	{
+		GObject o = getElementAt(x, y);
+		if(o instanceof TouchButton)
+		{
+			((TouchButton) o).releaseButton();
+		}
 	}
 	
 	private List<GenericButton> buttons;
