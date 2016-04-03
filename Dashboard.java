d3c7f77e090c7cf96d7dc803c7f32fbe04bc8edb
S <s>
2016-04-02 19:05:14 -0700

    {"types":"C","runs":0}

--- Dashboard.java
+++ Dashboard.java
@@ -6,14 +6,30 @@
  * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
  */
 
+import java.awt.Color;
+
 import acm.graphics.GCanvas;
 import acm.io.IOConsole;
 import acm.program.*;
 
-public class Dashboard extends GraphicsProgram {
-	public void run() {
-		
+public class Dashboard extends GraphicsProgram
+{
+	public void run()
+	{
+		rcl = new ToggleButton(BUTTON_WIDTH, BUTTON_HEIGHT, Color.RED, "RCL");
+		dst = new ToggleButton(BUTTON_WIDTH, BUTTON_HEIGHT, Color.RED, "RCL");
+		src = new ToggleButton(BUTTON_WIDTH, BUTTON_HEIGHT, Color.RED, "RCL");
+		add(rcl, BUTTON_SPACING, BUTTON_SPACING);
+		add(dst, BUTTON_SPACING, BUTTON_HEIGHT + 2 * BUTTON_SPACING);
+		add(src, BUTTON_SPACING, 2 * BUTTON_HEIGHT + 3 * BUTTON_SPACING);
 		
 	}
+	
+	private ToggleButton rcl;
+	private ToggleButton dst;
+	private ToggleButton src;
+	private static final int BUTTON_HEIGHT = 50;
+	private static final int BUTTON_WIDTH = 100;
+	private static final int BUTTON_SPACING = 10;
 }
 
