197f6141f0fa7ac20a7330e38c245ee0d01dbb1a
S <s>
2016-04-02 19:09:24 -0700

    {"types":"C","runs":2}

--- Dashboard.java
+++ Dashboard.java
@@ -7,8 +7,9 @@
  */
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 
-import acm.graphics.GCanvas;
+import acm.graphics.*;
 import acm.io.IOConsole;
 import acm.program.*;
 
@@ -22,6 +23,24 @@ public class Dashboard extends GraphicsProgram
 		add(rcl, BUTTON_SPACING, BUTTON_SPACING);
 		add(dst, BUTTON_SPACING, BUTTON_HEIGHT + 2 * BUTTON_SPACING);
 		add(src, BUTTON_SPACING, 2 * BUTTON_HEIGHT + 3 * BUTTON_SPACING);
+	}
+	
+	public void mousePressed(MouseEvent e)
+	{
+		GObject o = getElementAt(e.getX(), e.getY());
+		if(o instanceof ToggleButton)
+		{
+			((ToggleButton) o).pressButton();
+		}
+	}
+	
+	public void mouseReleased(MouseEvent e)
+	{
+		GObject o = getElementAt(e.getX(), e.getY());
+		if(o instanceof ToggleButton)
+		{
+			((ToggleButton) o).releaseButton();
+		}
 	}
 	
 	private ToggleButton rcl;
