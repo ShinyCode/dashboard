de2d54c517ba6fda55eb7625bb19db06164f8583
S <s>
2016-04-02 20:59:29 -0700

    {"types":"C","runs":27}

--- MouseWidget.java
+++ MouseWidget.java
@@ -1,4 +1,41 @@
 
-public class MouseWidget {
+import acm.graphics.*;
+import java.awt.event.*;
 
+public abstract class MouseWidget extends GCompound
+{
+	public void mousePressed(MouseEvent e)
+	{
+		
+	}
+	
+	public void mouseReleased(MouseEvent e)
+	{
+		
+	}
+	
+	public void mouseExited(MouseEvent e)
+	{
+		
+	}
+	
+	public void mouseClicked(MouseEvent e)
+	{
+		
+	}
+	
+	public void mouseEntered(MouseEvent e)
+	{
+		
+	}
+	
+	public void mouseDragged(MouseEvent e)
+	{
+		
+	}
+	
+	public void mouseMoved(MouseEvent e)
+	{
+		
+	}
 }
