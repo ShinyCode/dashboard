09eb5500f11f6101f3360c5b8cd5eac8e3462acd
S <s>
2016-04-04 14:34:04 -0700

    {"types":"AR","runs":54}

---
+++ InstrReadoutControl.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
+
+public class InstrReadoutControl extends ButtonGrid
+{
+	public InstrReadoutControl(double width, double height, double spacing, Color baseColor, Color buttonColor)
+	{
+		super(width, height, 4, 1, spacing, baseColor);
+	}
+}
