7bff98278a4235bc6a47454438b0d81a7bacb083
S <s>
2016-04-04 09:55:05 -0700

    {"types":"C","runs":3}

--- InstructionReadout.java
+++ InstructionReadout.java
@@ -1,4 +1,5 @@
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,9 +7,21 @@ import acm.graphics.*;
 
 public class InstructionReadout extends GCompound implements Incrementable
 {
-	public InstructionReadout(double width, double height, double spacing, double baseColor, double backColor, double barColor)
+	public InstructionReadout(double width, double height, double spacing, Color baseColor, Color backColor, Color barColor)
 	{
 		messages = new ArrayList<GLabel>();
+		
+		base = new GRect(width, height);
+		base.setFilled(true);
+		base.setFillColor(baseColor);
+		add(base, 0, 0);
+		
+		back = new GRect(width - 2 * spacing, height - 2 * spacing);
+		back.setFilled(true);
+		back.setFillColor(backColor);
+		add(back, spacing, spacing);
+		
+		
 	}
 	
 	public void increment()
