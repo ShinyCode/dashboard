e0ce0cdf0d0ee414d05fd5514ecc692369eada0b
S <s>
2016-04-04 14:36:58 -0700

    {"types":"C","runs":54}

--- InstrReadoutControl.java
+++ InstrReadoutControl.java
@@ -7,5 +7,16 @@ public class InstrReadoutControl extends ButtonGrid
 	public InstrReadoutControl(double width, double height, double spacing, Color baseColor, Color buttonColor)
 	{
 		super(width, height, 4, 1, spacing, baseColor);
+		clsButton = new TouchButton(width, height, buttonColor, "CLS");
+		add(clsButton, 0, 0);
+		frzButton = new ToggleButton(width, height, buttonColor, "FRZ");
+		add(frzButton, 1, 0);
+		incButton = new TouchButton(width, height, buttonColor, "INC");
+		add(incButton, 2, 0);
+		decButton = new TouchButton(width, height, buttonColor, "DEC");
+		add(decButton, 3, 0);
 	}
+	
+	private TouchButton incButton, decButton, clsButton;
+	private ToggleButton frzButton;
 }
