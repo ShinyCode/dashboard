b3ef00545e86fd5c50254426cd72d2c434246d03
S <s>
2016-04-03 19:13:31 -0700

    {"types":"C","runs":6}

--- LevelReadout.java
+++ LevelReadout.java
@@ -1,4 +1,30 @@
+import java.awt.Color;
+import acm.graphics.*;
 
-public class LevelReadout {
 
+public class LevelReadout extends GCompound implements Incrementable
+{
+	public LevelReadout(double width, double height, double spacing, Color baseColor, Color backColor, Color barColor)
+	{
+		
+	}
+	
+	public void increment()
+	{
+		
+	}
+	
+	public void decrement()
+	{
+		
+	}
+	
+	public void setLevel(int level)
+	{
+		
+	}
+	
+	int level;
+	int maxLevel;
+	int interval;
 }
