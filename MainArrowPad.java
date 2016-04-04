9605352962c39a0e244af47dbd6a85bfcfe2bcf5
S <s>
2016-04-03 23:10:13 -0700

    {"types":"C","runs":16}

--- MainArrowPad.java
+++ MainArrowPad.java
@@ -10,12 +10,20 @@ public class MainArrowPad extends ButtonGrid
 		addButton(aillButton, 0, 0);
 		fwdButton = new TouchButton(width, height, buttonColor, "FWD");
 		addButton(fwdButton, 0, 1);
-		revButton = new TouchButton(width, height, buttonColor, "REV");
-		addButton(revButton, 1, 1);
+		ailrButton = new TouchButton(width, height, buttonColor, "AILR");
+		addButton(fwdButton, 0, 2);
 		bnlButton = new TouchButton(width, height, buttonColor, "BNL");
 		addButton(bnlButton, 1, 0);
+		revButton = new TouchButton(width, height, buttonColor, "REV");
+		addButton(revButton, 1, 1);
 		bnrButton = new TouchButton(width, height, buttonColor, "BNR");
 		addButton(bnrButton, 1, 2);
+		rudlButton = new TouchButton(width, height, buttonColor, "RUDL");
+		addButton(rudlButton, 2, 0);
+		mimButton = new ToggleButton(width, height, buttonColor, "MIM");
+		addButton(mimButton, 2, 1);
+		rudrButton = new TouchButton(width, height, buttonColor, "RUDR");
+		addButton(rudrButton, 2, 2);
 	}
 	
 	private TouchButton aillButton, fwdButton, ailrButton, bnlButton, revButton, bnrButton, rudlButton, rudrButton;
