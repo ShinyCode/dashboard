5991f07f8a6b51be8571ac874836f6ad447b9c0f
S <s>
2016-04-04 19:11:48 -0700

    {"types":"C","runs":4}

--- Dashboard.java
+++ Dashboard.java
@@ -18,42 +18,7 @@ public class Dashboard extends GraphicsProgram
 	public void run()
 	{
 		addMouseListeners();
-		rcl = new TouchButton(BUTTON_WIDTH, BUTTON_HEIGHT, Color.RED, "RCL");
-		dst = new TouchButton(BUTTON_WIDTH, BUTTON_HEIGHT, Color.RED, "DST");
-		src = new TouchButton(BUTTON_WIDTH, BUTTON_HEIGHT, Color.RED, "SRC");
-		pwr = new ToggleButton(BUTTON_WIDTH, BUTTON_HEIGHT, Color.BLUE, "PWR");
-		bg = new ButtonGroup(BUTTON_WIDTH + 2 * BUTTON_SPACING, 3 * BUTTON_HEIGHT + 4 * BUTTON_SPACING, Color.BLACK);
-		bg.add(new TouchButton(BUTTON_WIDTH, BUTTON_HEIGHT, Color.GREEN, "INC"), BUTTON_SPACING, BUTTON_SPACING);
-		bg.add(new TouchButton(BUTTON_WIDTH, BUTTON_HEIGHT, Color.RED, "DEC"), BUTTON_SPACING, 2 * BUTTON_SPACING + BUTTON_HEIGHT);
-		bg.add(new ToggleButton(BUTTON_WIDTH, BUTTON_HEIGHT, Color.ORANGE, "SEL"), BUTTON_SPACING, 3 * BUTTON_SPACING + 2 * BUTTON_HEIGHT);
-		add(bg, 0, 4 * BUTTON_HEIGHT + 4 * BUTTON_SPACING);
-		bgr = new ButtonGrid(2 * BUTTON_WIDTH + 3 * BUTTON_SPACING, 2 * BUTTON_HEIGHT + 3 * BUTTON_SPACING, 2, 2, BUTTON_SPACING, Color.BLACK);
-		bgr.addButton(rcl, 0, 0);
-		bgr.addButton(dst, 1, 0);
-		bgr.addButton(src, 0, 1);
-		bgr.addButton(pwr, 1, 1);
-		add(bgr, 0, 0);
-		lr0 = new LevelReadout(30, 100, BUTTON_SPACING, 10, Color.BLACK, Color.GREEN, Color.RED);
-		add(lr0, 300, 0);
-		lr1 = new LevelReadout(30, 100, BUTTON_SPACING, 10, Color.BLACK, Color.GREEN, Color.RED);
-		add(lr1, 330, 0);
-		lr2 = new LevelReadout(30, 100, BUTTON_SPACING, 10, Color.BLACK, Color.GREEN, Color.RED);
-		add(lr2, 360, 0);
-		inc = new MultiIncrementer(BUTTON_WIDTH + 2 * BUTTON_SPACING, 3 * BUTTON_HEIGHT + 4 * BUTTON_SPACING, BUTTON_SPACING, Color.BLACK, Color.ORANGE);
-		add(inc, 200, 200);
-		inc.addIncrementable(lr0);
-		inc.addIncrementable(lr1);
-		inc.addIncrementable(lr2);
-		aap = new AuxArrowPad(3 * BUTTON_WIDTH + 4 * BUTTON_SPACING, 2 * BUTTON_HEIGHT + 3 * BUTTON_SPACING, BUTTON_SPACING, Color.BLACK, Color.MAGENTA);
-		add(aap, 300, 250);
-		map = new MainArrowPad(3 * BUTTON_WIDTH + 4 * BUTTON_SPACING, 3 * BUTTON_HEIGHT + 4 * BUTTON_SPACING, BUTTON_SPACING, Color.BLACK, Color.MAGENTA);
-		add(map, 400, 100);
-		map.addAuxArrowPad(aap);
-		ir = new InstrReadout(80, 200, BUTTON_SPACING, Color.BLACK, Color.ORANGE, Color.ORANGE.brighter());
-		add(ir, 400, 100);
-		irc = new InstrReadoutControl(BUTTON_WIDTH + 2 * BUTTON_SPACING, 4 * BUTTON_HEIGHT + 5 * BUTTON_SPACING, BUTTON_SPACING, Color.BLACK, Color.ORANGE);
-		add(irc, 200, 300);
-		irc.addInstrReadout(ir);
+		
 	}
 	
 	public void mousePressed(MouseEvent e)
@@ -70,19 +35,6 @@ public class Dashboard extends GraphicsProgram
 		if(o instanceof MouseWidget) ((MouseWidget) o).mouseReleased(e);
 	}
 	
-	private TouchButton rcl;
-	private TouchButton dst;
-	private TouchButton src;
-	private ToggleButton pwr;
-	private ButtonGroup bg;
-	private MultiIncrementer inc;
-	private ButtonGrid bgr;
-	private LevelReadout lr0;
-	private LevelReadout lr1;
-	private LevelReadout lr2;
-	private AuxArrowPad aap;
-	private MainArrowPad map;
-	private InstrReadout ir;
 	private InstrReadoutControl irc;
 	private static final int BUTTON_HEIGHT = 60;
 	private static final int BUTTON_WIDTH = 80;
