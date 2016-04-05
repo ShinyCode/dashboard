aeef395bbffbe27e7879409bdcddffc3ca232317
S <s>
2016-04-04 20:19:14 -0700

    {"types":"C","runs":15}

--- Sensor.java
+++ Sensor.java
@@ -3,5 +3,15 @@ import acm.graphics.*;
 
 public abstract class Sensor extends GCompound
 {
+	public Sensor()
+	{
+		
+	}
 	
+	public void setActive(boolean flag)
+	{
+		
+	}
+	
+	List<Readout> readouts;
 }
