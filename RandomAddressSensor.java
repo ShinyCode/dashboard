dddfcf95280d7cfb3786864ac63821330a7f84a4
S <s>
2016-04-07 13:02:05 -0700

    {"types":"C","runs":0}

--- RandomAddressSensor.java
+++ RandomAddressSensor.java
@@ -1,4 +1,25 @@
+import java.util.Random;
 
-public class RandomAddressSensor {
 
+public class RandomAddressSensor extends Sensor
+{
+	public RandomAddressSensor(int updateDelay)
+	{
+		super(updateDelay);
+		r = new Random();
+	}
+	
+	public String getReading()
+	{
+		String address = PREFIX;
+		for(int i = 0; i < NUM_DIGITS; ++i)
+		{
+			address += r.nextInt(10);
+		}
+		return address;
+	}
+	
+	private static final String PREFIX = "0x";
+	private static final int NUM_DIGITS = 8;
+	private Random r;
 }
