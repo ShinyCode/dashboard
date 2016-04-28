981f431afba5f679d859db47eee2e7b76b569455
S <s>
2016-04-27 19:10:49 -0700

    {"types":"C","runs":3}

--- Dashboard.java
+++ Dashboard.java
@@ -107,7 +107,7 @@ public class Dashboard extends GraphicsProgram
 		add(background, 0, 0);
 		background.sendToBack();
 		
-		prc = new Processor(1000);
+		prc = new Processor(1);
 		prc.setActive(true);
 	}
 	
