14d9d4e7129ebcf6b6579bbb25a913db1eb6262e
S <s>
2016-04-27 18:56:07 -0700

    {"types":"AC","runs":0}

--- Processor.java
+++ Processor.java
@@ -1,4 +1,48 @@
 
 public class Processor {
 
+	public void setActive(boolean flag)
+	{
+		if(flag && !active)
+		{
+			active = true;
+			turnOn();
+		}
+		else if(!flag && active)
+		{
+			active = false;
+			turnOff();
+		}
+	}
+	
+	private void turnOn()
+	{
+		thr = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				while(!Thread.interrupted())
+				{	
+					try
+					{
+						// Execute code
+					}
+					catch(InterruptedException e)
+					{
+						return;
+					}
+					
+				}
+			}
+		});
+		thr.start();
+	}
+	
+	private void turnOff()
+	{
+		thr.interrupt();
+	}
+	
+	private Thread thr;
+	private boolean active = false;
 }
