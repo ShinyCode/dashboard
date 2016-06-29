0a79fcf1fcadb98c57c5cdad209a69f6181e2661
S <s>
2016-06-28 20:16:54 -0400

    {"types":"C","runs":7}

--- CustomButtonGrid.java
+++ CustomButtonGrid.java
@@ -10,4 +10,23 @@ public final class CustomButtonGrid extends ButtonGrid
 	{
 		super(width, height, numRows, numCols, spacing, baseColor);
 	}
+	
+	public boolean addButton(Button gb, int row, int col)
+	{
+		return addButton(gb, row, col, 1, 1);
+	}
+	
+	public boolean addButton(Button gb, int row, int col, int rowSpan, int colSpan)
+	{
+		if(row < 0 || col < 0 || row >= numRows || col >= numCols) return false; //Invalid row, col dimension
+		if(rowSpan <= 0 || colSpan <= 0 || rowSpan > numRows - row || colSpan > numCols - col) return false; //Invalid row/column Span
+		gb.resize(colSpan * buttonWidth + (colSpan - 1) * spacing, rowSpan * buttonHeight + (rowSpan - 1) * spacing);
+		add(gb, col * buttonWidth + (col + 1) * spacing, row * buttonHeight + (row + 1) * spacing);
+		return true;
+	}
+	
+	public Button getButton(int row, int col)
+	{
+		return super.getButton(row, col);
+	}
 }
