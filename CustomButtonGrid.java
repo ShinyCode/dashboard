3d8b21114f0a8ad1f240d30243f5ff6d83236eaf
S <s>
2016-06-28 20:12:11 -0400

    {"types":"C","runs":7}

--- CustomButtonGrid.java
+++ CustomButtonGrid.java
@@ -1,5 +1,13 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
+import acm.graphics.GRect;
+
 
 public final class CustomButtonGrid extends ButtonGrid
 {
-
+	protected CustomButtonGrid(double width, double height, int numRows, int numCols, double spacing, Color baseColor)
+	{
+		super(width, height, numRows, numCols, spacing, baseColor);
+	}
 }
