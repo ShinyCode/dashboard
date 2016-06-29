cccb4e3719ee7eb57f38d3e990f080ae05a6a6df
S <s>
2016-06-28 22:10:41 -0400

    {"types":"AC","runs":9}

--- CustomButtonGroup.java
+++ CustomButtonGroup.java
@@ -1,5 +1,16 @@
 
 public final class CustomButtonGroup extends ButtonGroup
 {
-	public static 
+	public static final class Builder extends ButtonGroup.Builder<Builder>
+	{
+		public Builder(double width, double height)
+		{
+			super(width, height);
+		}
+		
+		public CustomButtonGroup build()
+		{
+			return new CustomButtonGrid(width, height, numRows, numCols, spacing, baseColor);
+		}		
+	}
 }
