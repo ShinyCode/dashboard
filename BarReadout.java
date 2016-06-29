68fb8b18662053667bc3f756d652c17653247db8
S <s>
2016-06-29 19:18:32 -0400

    {"types":"C","runs":15}

--- BarReadout.java
+++ BarReadout.java
@@ -15,7 +15,7 @@ public final class BarReadout extends Readout implements Incrementable, NumberUp
 	private double maxValue;
 	
 	public static final int VERTICAL = 0;
-	public static final int HORIZONTAL = 0;
+	public static final int HORIZONTAL = 1;
 	
 	public static final class Builder extends Readout.Builder<Builder>
 	{	
