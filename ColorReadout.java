a164ec1cafcffe9acf5643050f33af8b9609cbd6
S <s>
2016-06-29 12:11:31 -0400

    {"types":"C","runs":0}

--- ColorReadout.java
+++ ColorReadout.java
@@ -1,5 +1,28 @@
+import java.awt.Color;
 
-public class ColorReadout extends Readout
+import acm.graphics.GRect;
+
+
+public final class ColorReadout extends Readout
 {
+	private GRect base;
+	private GRect colorDisplay;
 	
+	protected ColorReadout(double width, double height, double spacing, Color baseColor, Color defaultColor)
+	{
+		base = new GRect(width, height);
+		base.setFilled(true);
+		base.setFillColor(baseColor);
+		add(base, 0, 0);
+		
+		colorDisplay = new GRect(width - 2 * spacing, height - 2 * spacing);
+		colorDisplay.setFilled(true);
+		colorDisplay.setFillColor(defaultColor);
+		add(colorDisplay, spacing, spacing);
+	}
+	
+	public void update(Color newColor)
+	{
+		colorDisplay.setFillColor(newColor);
+	}
 }
