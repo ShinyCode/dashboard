5afc17743b7fa2291f76932755440b5754827488
S <s>
2016-06-29 10:30:55 -0400

    {"types":"AC","runs":0}

--- Readout.java
+++ Readout.java
@@ -1,7 +1,46 @@
+import java.awt.Color;
+
 import acm.graphics.GCompound;
 
 
-public abstract class Readout extends GCompound implements Updatable
+public abstract class Readout extends GCompound
 {
-
+	@SuppressWarnings("rawtypes")
+	public static abstract class Builder<T extends Builder>
+	{
+		protected final double width;
+		protected final double height;
+		
+		protected int numRows = 1;
+		protected int numCols = 1;
+		protected double spacing = 0;
+		protected Color baseColor = Color.BLACK;
+		
+		public Builder(double width, double height)
+		{
+			this.width = width;
+			this.height = height;
+		}
+		
+		public T withRowsCols(int numRows, int numCols)
+		{
+			this.numRows = numRows;
+			this.numCols = numCols;
+			return (T)this;
+		}
+		
+		public T withSpacing(double spacing)
+		{
+			this.spacing = spacing;
+			return (T)this;
+		}
+		
+		public T withBaseColor(Color baseColor)
+		{
+			this.baseColor = baseColor;
+			return (T)this;
+		}
+		
+		public abstract Readout build();			
+	}
 }
