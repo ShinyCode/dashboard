35f7186abc9fdb40805cb71dacd487aef9f8763a
S <s>
2016-06-30 12:10:18 -0400

    {"types":"C","runs":103}

--- DialReadout.java
+++ DialReadout.java
@@ -112,17 +112,15 @@ public final class DialReadout extends Readout
 		add(dial, xDisp - (bounds.getX() - dial.getX()), yDisp - (bounds.getY() - dial.getY()));
 		
 		// Draw the dot to mark the start of the dial
-		/*
 		GRectangle frame = dial.getFrameRectangle();
 		dialCenter = new GPoint(frame.getX() + frame.getWidth() / 2, frame.getY() + frame.getHeight() / 2);
-		GOval dot = new GOval(spacing / 2, spacing / 2);
-		dot.setFilled(true);
-		dot.setFillColor(color);
-		double dotX = dialCenter.getX() - dot.getWidth() / 2 + (frame.getWidth() + spacing) * Math.cos(Math.toRadians(startAngle)) / 2;
-		double dotY = dialCenter.getY() - dot.getHeight() / 2 - (frame.getHeight() + spacing) * Math.sin(Math.toRadians(startAngle)) / 2;
-		add(dot, dotX, dotY);
-		setLevel(0);
-		*/
+		// GOval dot = new GOval(spacing / 2, spacing / 2);
+		// dot.setFilled(true);
+		// dot.setFillColor(color);
+		// double dotX = dialCenter.getX() - dot.getWidth() / 2 + (frame.getWidth() + spacing) * Math.cos(Math.toRadians(startAngle)) / 2;
+		// double dotY = dialCenter.getY() - dot.getHeight() / 2 - (frame.getHeight() + spacing) * Math.sin(Math.toRadians(startAngle)) / 2;
+		// add(dot, dotX, dotY);
+		// setLevel(0);
 		
 		// Draw the needle on the dial face
 		needle = new GLine(dialCenter.getX(),
