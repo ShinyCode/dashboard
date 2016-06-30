55dc1c18c3e6cd67365171debdc347c3be50cb52
S <s>
2016-06-30 18:09:44 -0400

    {"types":"C","runs":129}

--- NumberReadout.java
+++ NumberReadout.java
@@ -6,4 +6,37 @@ public abstract class NumberReadout extends Readout implements Incrementable, Nu
 	protected int numDivisions;
 	protected double minValue;
 	protected double maxValue;
+	
+	public void increment()
+	{
+		setLevel(level + 1);
+	}
+	
+	public void decrement()
+	{
+		setLevel(level - 1);
+	}
+	
+	public void update(double value)
+	{
+		if(value > maxValue)
+		{
+			setLevel(numDivisions);
+			return;
+		}
+		if(value < minValue)
+		{
+			setLevel(0);
+			return;
+		}
+		double range = maxValue - minValue;
+		// TODO: Check if want to make long
+		setLevel((int)Math.round((value - minValue) * numDivisions / range));
+	}
+	
+	public void setLevel(int level)
+	{
+		if(level < 0 || level > numDivisions) return;
+		this.level = level;
+	}
 }
