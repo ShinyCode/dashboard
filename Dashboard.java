b1b6afc223bc0fc554e3de0e2cc19224f239a8ff
S <s>
2016-06-30 12:17:48 -0400

    {"types":"C","runs":104}

--- Dashboard.java
+++ Dashboard.java
@@ -160,7 +160,7 @@ public class Dashboard extends GraphicsProgram
 		add(r, aap.getX(), aap.getY() + aap.getHeight());
 		*/
 		add(dr, aap.getX(), aap.getY() + aap.getHeight());
-		dr.setLevel(90);
+		dr.setLevel(25);
 		
 		// Add a background
 		GRect background = new GRect(addr.getX() + addr.getWidth() + 2 * COMPONENT_SPACING - pwr.getX(),
