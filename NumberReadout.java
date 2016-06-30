6ce206ca0baa59cae453a4274128a99b6fa20c78
S <s>
2016-06-30 18:06:53 -0400

    {"types":"C","runs":129}

--- NumberReadout.java
+++ NumberReadout.java
@@ -4,4 +4,6 @@ public abstract class NumberReadout extends Readout implements Incrementable, Nu
 {
 	protected int level;
 	protected int numDivisions;
+	protected double minValue;
+	protected double maxValue;
 }
