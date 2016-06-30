2be94d7cfe1b57a8d6597074a7199ea455dfa052
S <s>
2016-06-29 23:12:22 -0400

    {"types":"C","runs":34}

--- DialReadout.java
+++ DialReadout.java
@@ -86,7 +86,7 @@ public final class DialReadout extends Readout
 		System.out.println((width - 2 * spacing) + " " + (height - 2 * spacing));
 		dial.setFilled(true);
 		dial.setFillColor(color);
-		add(dial, spacing + 30, spacing);
+		add(dial, spacing, spacing);
 		
 		setLevel(0);
 	}
