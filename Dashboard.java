80cb571c31af5e502cd7ff871666e0fcc3ac1849
S <s>
2016-06-30 23:28:01 -0400

    {"types":"C","runs":129}

--- Dashboard.java
+++ Dashboard.java
@@ -1,10 +1,4 @@
-/*
- * File: BlankClass.java
- * ---------------------
- * This class is a blank one that you can change at will. Remember, if you change
- * the class name, you'll need to change the filename so that it matches.
- * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
- */
+package outside;
 
 import java.awt.Color;
 import java.awt.event.MouseEvent;
